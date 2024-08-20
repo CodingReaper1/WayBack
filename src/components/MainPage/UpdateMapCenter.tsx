15f4a86954b5316b6fd0d1523b07401791e0694a
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-08-20 12:37:02 +0400

    full typescript convertion + cleanup

--- src/components/MainPage/UpdateMapCenter.tsx
+++ src/components/MainPage/UpdateMapCenter.tsx
@@ -2,20 +2,20 @@
 //   return null;
 // }
 
-import { useCallback } from "react";
+// import { useCallback } from "react";
 
-type UpdateMapCenterTypes = {
-  position: [number, number];
-};
+// type UpdateMapCenterTypes = {
+//   position: [number, number];
+// };
 
-const UpdateMapCenter = useCallback(
-  ({ position }: UpdateMapCenterTypes) => {
-    if (!map || !myPosition || !routeLocked) return;
-    map.panTo(myPosition, map.getZoom(), { animate: true, duration: 1 });
-    return null;
-  },
-  // [myPosition, routeLocked, map],
-  [routeLocked, map, myPosition],
-);
+// const UpdateMapCenter = useCallback(
+//   ({ position }: UpdateMapCenterTypes) => {
+//     if (!map || !myPosition || !routeLocked) return;
+//     map.panTo(myPosition, map.getZoom(), { animate: true, duration: 1 });
+//     return null;
+//   },
+//   // [myPosition, routeLocked, map],
+//   [routeLocked, map, myPosition],
+// );
 
-export default UpdateMapCenter;
+// export default UpdateMapCenter;
