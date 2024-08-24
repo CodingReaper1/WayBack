bd950b3cc4921bec8ed893574596e178ec4aa803
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-08-24 19:39:39 +0400

    fixing login responsive

--- src/trash/Overlay.tsx
+++ src/trash/Overlay.tsx
@@ -1,6 +1,6 @@
 import { createPortal } from "react-dom";
 
-import useMainPageContext from "../context/useMainPageContext";
+// import useMainPageContext from "../context/useMainPageContext";
 
 function Overlay() {
   // const { closeModal } = useMainPageContext();
@@ -8,7 +8,7 @@ function Overlay() {
   return createPortal(
     <div
       className="absolute bottom-0 left-0 right-0 top-0 z-[999999999999999999999999999] "
-      onClick={() => closeModal()}
+      // onClick={() => closeModal()}
     ></div>,
     document.body,
   );
