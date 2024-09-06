ef2d63bfe42c0ab654d5407c6ab64bf3eb927e0f
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-09-06 17:09:32 +0400

    finnally new location finder

--- src/services/apiUserLocation.ts
+++ src/services/apiUserLocation.ts
@@ -1,11 +1,19 @@
 import Apiip from "apiip.net";
+import toast from "react-hot-toast";
 
-export async function findUserLocationApi(): Promise<[number, number]> {
-  // @ts-expect-error |||| doesnt seem like error its just annyoing
-  const apiip = Apiip(import.meta.env.VITE_APIIP_API_KEY);
+export async function findUserLocationApi(): Promise<
+  [number, number] | undefined
+> {
+  try {
+    // @ts-expect-error |||| doesnt seem like error its just annyoing
+    const apiip = Apiip(import.meta.env.VITE_APIIP_API_KEY);
 
-  const data = await apiip.getLocation();
-  console.log(data);
+    const data = await apiip.getLocation();
+    console.log(data);
 
-  return [data.latitude, data.longitude];
+    return [data.latitude, data.longitude];
+  } catch (err) {
+    const error = err as Error;
+    toast.error(error.message);
+  }
 }
