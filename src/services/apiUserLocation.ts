f87a7abf80e1bc2ff622f8dc49dddaa32dc69b31
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-09-06 17:14:08 +0400

    finnally new location finder

--- src/services/apiUserLocation.ts
+++ src/services/apiUserLocation.ts
@@ -1,7 +1,7 @@
 import Apiip from "apiip.net";
 import toast from "react-hot-toast";
 
-export async function findUserLocationApi(): Promise<[number, number]> {
+export async function findUserLocationApi() {
   try {
     // @ts-expect-error |||| doesnt seem like error its just annyoing
     const apiip = Apiip(import.meta.env.VITE_APIIP_API_KEY);
